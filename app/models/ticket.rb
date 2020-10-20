fb965cf34847043f62a6450f5dc02042298b33dd
victorscholz <vgscholz@gmail.com>
2020-10-20 15:52:30 -0400

    spacing refactor

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,31 +1,4 @@
 class Ticket < ActiveRecord::Base
-<<<<<<< HEAD
-    belongs_to :user
-    belongs_to :event
-<<<<<<< HEAD
-=======
-    after_initialize :init
-
-    def init
-      self.ticket_count  ||= 0
-    end
-<<<<<<< HEAD
->>>>>>> second_repo
-=======
-
-<<<<<<< HEAD
-    # def user_ticket
-    #     Ticket.all.select {|ticket| ticket.user_id == self.id}
-    # end
-
-    # def event_ticket
-    #   Ticket.all.select {|ticket| ticket.event_id == self.id}
-    # end
->>>>>>> second_repo
-=======
->>>>>>> second_repo
-end
-=======
   belongs_to :user
   belongs_to :event
   after_initialize :init
@@ -34,4 +7,3 @@ end
     self.ticket_count ||= 0
   end
 end
->>>>>>> second_repo
