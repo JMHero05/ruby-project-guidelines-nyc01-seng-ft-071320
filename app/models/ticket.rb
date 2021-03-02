cb3b3045438602c184a7722da5af7fd018565cc8
Jake Mills <jake.mills.05@gmail.com>
2021-03-02 16:22:03 -0500

    Never commited deletion of commented out code

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,26 +1,10 @@
 class Ticket < ActiveRecord::Base
     belongs_to :user
     belongs_to :event
-<<<<<<< HEAD
-=======
     after_initialize :init
 
     def init
       self.ticket_count  ||= 0
     end
-<<<<<<< HEAD
->>>>>>> second_repo
-=======
-
-<<<<<<< HEAD
-    # def user_ticket
-    #     Ticket.all.select {|ticket| ticket.user_id == self.id}
-    # end
-
-    # def event_ticket
-    #   Ticket.all.select {|ticket| ticket.event_id == self.id}
-    # end
->>>>>>> second_repo
-=======
->>>>>>> second_repo
+    
 end
