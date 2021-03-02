869cc6fb37a2204a29f069fcdbbcf6c1efc839ad
Jake Mills <jake.mills.05@gmail.com>
2021-03-02 16:22:46 -0500

    Never commited changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 2020_07_29_042134) do
-=======
-ActiveRecord::Schema.define(version: 2020_07_30_195941) do
->>>>>>> second_repo
-=======
-ActiveRecord::Schema.define(version: 2020_07_30_215848) do
->>>>>>> second_repo
-=======
 ActiveRecord::Schema.define(version: 2020_07_31_002750) do
->>>>>>> second_repo
 
   create_table "events", force: :cascade do |t|
     t.string "event_name"
@@ -32,25 +20,16 @@ ActiveRecord::Schema.define(version: 2020_07_31_002750) do
     t.string "venue_city"
     t.string "venue_state"
     t.string "postal_code"
-<<<<<<< HEAD
-=======
     t.integer "ticket_count"
->>>>>>> second_repo
   end
 
   create_table "tickets", force: :cascade do |t|
     t.integer "user_id"
     t.integer "event_id"
-<<<<<<< HEAD
-=======
     t.integer "ticket_count"
-<<<<<<< HEAD
->>>>>>> second_repo
-=======
     t.string "event_name"
     t.string "date"
     t.string "venue_name"
->>>>>>> second_repo
   end
 
   create_table "users", force: :cascade do |t|
